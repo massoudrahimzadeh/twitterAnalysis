6a547575d9596829c41d9ba2153cdc503aeefda7
Massoud <massoudrmz@gmail.com>
2020-06-09 21:56:27 +0200

    task was assigned and teammates were added, I have changed the structure and create a class: TwitterAnalyze to make the code easier to read

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,74 +1,15 @@
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CoreMap;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-
-class TwitterAnalyzer {
 
 
-    private static SentimentValue getSentiment(String tweet) {
-
-    }
-    private static List<Status> searchTweets(String query) {
-// Aswin sentiment filter
-    }
-    private static List<Status> mostUsedEmojis() {
-// Massoud
-    }
-    private static List<Status> mostActiveUser(){
-// Burak
-    }
-    private static String mostMentionedWord(List<Status> tweets) {
-// Massoud
-    }
-
-
-    private static List<Status> filterTweetsBasedOnSentiment(SentimentValue sentiment, List<Status> tweets) {
-        ArrayList<Status> result = new ArrayList<Status>();
-        for (Status tweet: tweets) {
-            if (getSentiment(tweet.getText()) == sentiment) {
-                result.add(tweet);
-            }
-        }
-
-        return result;
-    }
-
-}
 
 public class Main {
-    private static void analyzeTweet(String tweet) {
-        Document doc = new Document(tweet);
-        System.out.println("#1 " + tweet + ": " + doc.sentences().size());
-
-    }
-    public static SentimentValue sentimentAnalyse(String tweet) {
 
 
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos, parse, sentiment");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        Annotation annotation = pipeline.process(tweet);
-        for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-            Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
-            int value = RNNCoreAnnotations.getPredictedClass(tree);
-            SentimentValue sentimentValue = SentimentValue.fromValue(value);
-            System.out.println("Sentiment analysis of this tweet: "+ sentimentValue);
-            return sentimentValue;
-        }
-        return SentimentValue.NEGATIVE;
-    }
     public static void main(String[] args) {
+        TwitterAnalyzer ta = new TwitterAnalyzer();
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("YKE6GZQQb30QL1VmkNGZ9PDuc")
@@ -84,11 +25,12 @@ public class Main {
             List<Status> statuses = result.getTweets();
             System.out.println("Showing home timeline.");
             for (Status status : statuses) {
-                analyzeTweet(status.getText());
-                sentimentAnalyse(status.getText());
+                ta.analyzeTweet(status.getText());
+                ta.sentimentAnalyse(status.getText());
                 System.out.println("=========");
-
             }
+            System.out.println(ta.mostMentionedWord(statuses));
+            System.out.println(ta.mostUsedEmojis(statuses));
         } catch (TwitterException e) {
             e.printStackTrace();
         }
